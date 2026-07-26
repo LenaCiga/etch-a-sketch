87b84ad17f4c3f068cc9cbe82bcfe77e4d56572f
Lena <187453456+LenaCiga@users.noreply.github.com>
2026-07-26 21:53:26 +0200

    Add 16x16 div grid
    Through flex, styling for that and also started
    adding a button for new canvas

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const divParent = document.getElementById("container");
 const fragmentChildren = document.createDocumentFragment();
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
     const divChild = document.createElement("div");
     divChild.className = "child-div";
     divChild.textContent = `Div ${i + 1}`;
@@ -10,3 +10,18 @@ for (let i = 0; i < 16; i++) {
 }
 
 divParent.appendChild(fragmentChildren);
+
+divParent.addEventListener("mouseover", (e) => {
+    if (e.target.matches('.child-div')) { // Better than contains
+        e.target.classList.add('hover');
+    }
+});
+
+// divParent.addEventListener("mouseout", (e) => {
+//     if (e.target.matches('.child-div')) {
+//         e.target.classList.remove('hover');
+//     }
+// });  //if i want the regular hover I include this too
+
+const bttn = document.createElement("button");
+bttn.textContent = "New canvas size";
