8b13db9438996c7224123a7bebfd7dfedc1838fd
Lena <187453456+LenaCiga@users.noreply.github.com>
2026-07-27 22:52:40 +0200

    Add Create new canvas button
    And make it prompt for user number input for the
    creation of a new canvas grid

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const divParent = document.getElementById("container");
-const fragmentChildren = document.createDocumentFragment();
 
-for (let i = 0; i < 256; i++) {
-    const divChild = document.createElement("div");
-    divChild.className = "child-div";
-    divChild.textContent = `Div ${i + 1}`;
-
-    fragmentChildren.appendChild(divChild);
+function createDivs(count) {
+    const fragmentChildren = document.createDocumentFragment();
+    const grid = count * count;
+    const flexBasis = 100 / count;
+    
+    for (let i = 0; i < grid; i++) {
+        const divChild = document.createElement("div");
+        divChild.className = "child-div";
+        // divChild.textContent = `Div ${i + 1}`;
+        divChild.style.flex = `0 0 ${flexBasis}%`;
+        
+        fragmentChildren.appendChild(divChild);
+    }
+    
+    divParent.appendChild(fragmentChildren);
 }
 
-divParent.appendChild(fragmentChildren);
+createDivs(16);
 
 divParent.addEventListener("mouseover", (e) => {
     if (e.target.matches('.child-div')) { // Better than contains
@@ -24,4 +32,23 @@ divParent.addEventListener("mouseover", (e) => {
 // });  //if i want the regular hover I include this too
 
 const bttn = document.createElement("button");
+bttn.className = "clear-button";
 bttn.textContent = "New canvas size";
+document.body.appendChild(bttn);
+// button for creating new grid canvas which removes the previous 16x16 one
+
+bttn.addEventListener("click", () => {
+    const input = prompt("Enter a number N that will create N x N grid for drawing");
+
+    if (input !== null) {
+        const count = parseInt(input);
+
+        if (!isNaN(count) && count > 0 && count < 100) {
+            divParent.innerHTML = "";
+            createDivs(count);
+        }
+        else {
+            alert("Please enter valid numbers from 1 to 100");
+        }
+    }
+});
